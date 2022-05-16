bf7dcb0534764102850ca5264fe65f713e641d8d
Douglas Ramiro <douglars@amazon.com>
2022-05-16 14:55:22 -0300

    wip

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, jsonify, make_response
+import requests
+from flask_cors import CORS
+import boto3
+import socket
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/')
+def index():
+    response = make_response(
+        jsonify(
+            {"message": "Hello from app"}
+        ),
+        200,
+    )
+    response.headers["Content-Type"] = "application/json"
+    return response
+
+
+@app.route('/getTag', methods=['GET'])
+@app.route('/getTag/', methods=['GET'])
+def get_tag():
+    instance_id = (requests.get("http://169.254.169.254/latest/meta-data/instance-id")).text
+
+    ec2 = boto3.resource('ec2', region_name="us-east-1")
+    ec2instance = ec2.Instance(instance_id)
+    host = socket.gethostname();
+    server = {"name": "Tag not found!", "host": host}
+    found = False
+
+    for tags in ec2instance.tags:
+        print(tags)
+        if "categoria" in tags['Key']:
+            server = {"name": tags['Value'], "host": host}
+            found = True
+            break
+
+    codigo = 200 if found else 404
+
+    response = make_response(
+        jsonify(server),
+        codigo,
+    )
+    response.headers["Content-Type"] = "application/json"
+
+    return response
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
